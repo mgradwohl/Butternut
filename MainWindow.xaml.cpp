5c2c1cf8ce01ada54d4a24159ef09a75eb253ee9
Matt Gradwohl <mattgr@msn.com>
2023-11-30 14:11:06 -0800

    Updating nuget

--- MainWindow.xaml.cpp
+++ MainWindow.xaml.cpp
@@ -60,6 +60,8 @@ namespace winrt::Butternut::implementation
 
         //https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
         MainWindowT::InitializeComponent();
+        winrt::Microsoft::UI::Xaml::Media::MicaBackdrop backdrop;
+        SystemBackdrop(backdrop);
 
         PropertyChanged({ this, &MainWindow::OnPropertyChanged });
 
